1b8420c1508daaa33dc0db4c536b3ef8889cd08a
hubertbaron10 <hubert.baron10@gmail.com>
2021-03-10 13:44:30 +0000

    ostatnia zmiana

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
-  //zmiana pliku
+using namespace std;
+
+void intro()
+{
+  cout << "Hubert Baron" << endl; 
+}
+
+
+/* struct Field
+{
+ bool hasMine;
+ bool hasFlag;
+ bool isRevealed;
+};
+
+class MinesweaperBoard
+{
+  Field board[100][100];
+  int width;
+  int height;
+
+  public:
+  MinesweaperBoard();
+  void debug_display() const;
+};
+
+
+
+MinesweaperBoard::MinesweaperBoard() : height(10), width(10)
+{
+ 
+  
+}
+
+void MinesweaperBoard::debug_display() const
+{
+
+}
+*/
+int main() 
+{
+  intro();
+  return 0;
 }
