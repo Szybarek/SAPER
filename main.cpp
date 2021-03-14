2a895c134d8b38e29e63a9ee7a244ddbb679b749
hubertbaron10 <hubert.baron10@gmail.com>
2021-03-14 20:47:51 +0000

    przejrzystosc kodu

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 void intro()
 {
-  cout << "Hubert Baron" << endl;
+    cout << "Hubert Baron" << endl;
 }
 
 
@@ -30,7 +30,7 @@ class MinesweeperBoard
 
 public:
     MinesweeperBoard(int w, int h, GameMode mode);
-    int countMines(int row, int col) const;  
+    int countMines(int row, int col) const;
     bool hasFlag(int row, int col) const;
     void debug_display() const;
     int getMineCount() const;
@@ -40,83 +40,83 @@ public:
 };
 
 void MinesweeperBoard::toggleFlag(int row, int col, GameState status)
-{ 
- if(status == RUNNING && !board[row][col].isRevealed)
- {
- if(board[row][col].hasFlag)
- board[row][col].hasFlag = false;
- if(!board[row][col].hasFlag)
- board[row][col].hasFlag = true;
- }
+{
+    if(status == RUNNING && !board[row][col].isRevealed)
+    {
+        if(board[row][col].hasFlag)
+            board[row][col].hasFlag = false;
+        if(!board[row][col].hasFlag)
+            board[row][col].hasFlag = true;
+    }
 }
 
 int MinesweeperBoard::countMines(int row, int col) const
 {
-  int Mines = 0;
-  if(row >=1 && board[row-1][col].hasMine)
-  Mines = Mines + 1;
-  if(row >=1 && col >=1 && board[row-1][col-1].hasMine)
-  Mines = Mines + 1;
-  if(row >=1 && col < width && board[row-1][col+1].hasMine)
-  Mines = Mines + 1;
-  if(row < width && board[row+1][col].hasMine)
-  Mines = Mines + 1;
-  if(row < width && col >=1 && board[row+1][col-1].hasMine)
-  Mines = Mines + 1;
-  if(row < height && col < width && board[row+1][col+1].hasMine)
-  Mines = Mines + 1;
-  if(col >=1 && board[row][col-1].hasMine)
-  Mines = Mines + 1;
-  if(col < width && board[row][col+1].hasMine)
-  Mines = Mines + 1;
-  return Mines;
+    int Mines = 0;
+    if(row >=1 && board[row-1][col].hasMine)
+        Mines = Mines + 1;
+    if(row >=1 && col >=1 && board[row-1][col-1].hasMine)
+        Mines = Mines + 1;
+    if(row >=1 && col < width && board[row-1][col+1].hasMine)
+        Mines = Mines + 1;
+    if(row < width && board[row+1][col].hasMine)
+        Mines = Mines + 1;
+    if(row < width && col >=1 && board[row+1][col-1].hasMine)
+        Mines = Mines + 1;
+    if(row < height && col < width && board[row+1][col+1].hasMine)
+        Mines = Mines + 1;
+    if(col >=1 && board[row][col-1].hasMine)
+        Mines = Mines + 1;
+    if(col < width && board[row][col+1].hasMine)
+        Mines = Mines + 1;
+    return Mines;
 }
 
 bool MinesweeperBoard::hasFlag(int row, int col) const
 {
- if(board[row][col].hasFlag)
- return true;
- if(!board[row][col].hasFlag || board[row][col].isRevealed /* || + wyjscie poza tablice*/)
- return false;
- return 0;
+    if(board[row][col].hasFlag)
+        return true;
+    if(!board[row][col].hasFlag || board[row][col].isRevealed /* || + wyjscie poza tablice*/)
+        return false;
+    return 0;
 }
 
 MinesweeperBoard::MinesweeperBoard(int w, int h, GameMode mode) : height(h), width(w)
 {
- {
-   int MineAmount = height * width;
-   if(mode == EASY)
-   {
-   MineAmount = MineAmount * (0.1);
-   }
-   if(mode == NORMAL)
-   {
-   MineAmount = MineAmount * (0.2);
-   }
-   if(mode == HARD)
-   {
-   MineAmount = MineAmount * (0.3);
-   }
-   for(int y = 0; y < height; y++) 
     {
-     for(int x = 0; x < width; x++) 
-      {
-       board[x][y].hasMine = false;
-      }
+        int MineAmount = height * width;
+        if(mode == EASY)
+        {
+            MineAmount = MineAmount * (0.1);
+        }
+        if(mode == NORMAL)
+        {
+            MineAmount = MineAmount * (0.2);
+        }
+        if(mode == HARD)
+        {
+            MineAmount = MineAmount * (0.3);
+        }
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                board[x][y].hasMine = false;
+            }
+        }
+        srand(time(NULL));
+        for(int t = 0; t < MineAmount; t++)
+        {
+            int r1 = rand()%width;
+            int r2 = rand()%height;
+            while(board[r1][r2].hasMine)
+            {
+                r1 = rand()%width;
+                r2 = rand()%height;
+            }
+            board[r1][r2].hasMine = true;
+        }
     }
-   srand(time(NULL));
-   for(int t = 0; t < MineAmount; t++)
-    {
-     int r1 = rand()%width;
-     int r2 = rand()%height;
-      while(board[r1][r2].hasMine)
-      {
-       r1 = rand()%width;
-       r2 = rand()%height;
-      }
-      board[r1][r2].hasMine = true;
-    }
-  }
 }
 
 
@@ -147,25 +147,25 @@ void MinesweeperBoard::debug_display() const
 
 void MinesweeperBoard::revealField(int row, int col, GameState status)
 {
-  if(status == RUNNING && !board[row][col].isRevealed  && !board[row][col].hasFlag)
-  {
-    if(!board[row][col].hasMine)
-    board[row][col].isRevealed = true;
-    if(board[row][col].hasMine)
+    if(status == RUNNING && !board[row][col].isRevealed  && !board[row][col].hasFlag)
     {
-      //-jezeli jest to pierwsza akcja gracza to mina zawsze znajduje sie w innym miejscu (dodac)
-      board[row][col].isRevealed = true;
-      status = FINISHED_LOSS;
+        if(!board[row][col].hasMine)
+            board[row][col].isRevealed = true;
+        if(board[row][col].hasMine)
+        {
+            //-jezeli jest to pierwsza akcja gracza to mina zawsze znajduje sie w innym miejscu (dodac)
+            board[row][col].isRevealed = true;
+            status = FINISHED_LOSS;
+        }
     }
-  }
 }
 
 GameState Minesweeper::getGameState()
 {
-if(status == RUNNING)
-{
-  if(status ==)
-}
+    if(status == RUNNING)
+    {
+        if(status ==)
+    }
 }
 
 int main()
@@ -179,18 +179,18 @@ int main()
     start.revealField(2, 2, RUNNING);
     start.debug_display();
     getGameState();
-{
-if(RUNNING)
-{
-  
-}
-}
+    {
+        if(RUNNING)
+        {
+
+        }
+    }
 }
 
 #endif
 
-      /*board[1][1].hasMine = true;
-      board[1][2].hasMine = true;
-      board[1][0].hasMine = true;
-      board[2][1].hasMine = true;
-      board[2][1].hasFlag = true;*/
+/*board[1][1].hasMine = true;
+board[1][2].hasMine = true;
+board[1][0].hasMine = true;
+board[2][1].hasMine = true;
+board[2][1].hasFlag = true;*/
